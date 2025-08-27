3d9b572f89719e4e70e538c59e0fe0843c26760b
greg-is-kub <greg_is_kub@pm.me>
2025-08-27 16:33:24 +0200

    feat : removed pending actions from battlestate

--- pkmn_rl_arena/env/battle_state.py
+++ pkmn_rl_arena/env/battle_state.py
@@ -1,11 +1,10 @@
 from enum import Enum
 from dataclasses import dataclass
-from typing import Dict, Optional
-
 
 class TurnType(Enum):
     """Enumeration for different turn types"""
 
+    NOT_STARTED ="not_started"
     CREATE_TEAM = "create_team"  # Initial team creation
     GENERAL = "general"  # Both players act simultaneously
     PLAYER = "player"  # Only player acts
@@ -19,11 +18,6 @@ class BattleState:
 
     current_step: int = 0
     episode_done: bool = False
-    current_turn: Optional[TurnType] = None
+    current_turn: TurnType = TurnType.NOT_STARTED
     waiting_for_action: bool = False
     episode_steps: int = 0
-    pending_actions: Dict[str, Optional[int]] = None
-
-    def __post_init__(self):
-        if self.pending_actions is None:
-            self.pending_actions = {"player": None, "enemy": None}
